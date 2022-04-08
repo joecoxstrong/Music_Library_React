5b55c4814880b292ed34a4088d75219c6d5b602a
joecoxstrong <joecoxstrong@gmail.com>
2022-04-08 14:50:01 -0400

    Created useEffect function to link react to backend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
+import React, { useEffect, useState} from 'react'
+
+
 
 function App() {
+  const [songs, setSongs] = useState()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch('http://127.0.0.1:8000/api/songs/music/')
+      const jsonResult = await result.json()
+
+      setSongs(jsonResult)
+    }
+
+    fetchData()
+  }, [])
+
+  const submitSong = async () => {
+
+  }
   return (
     <div>
-     <h3>Hello there.</h3>
-    </div>
+      <button>Add New Song</button>
+     <h3>Songs</h3>
+     {songs.map(songs =>
+      <div key={songs.id} >
+        <h1>{songs.title}</h1>
+        <h3>{songs.artist}</h3>
+        <h3>{songs.album}</h3>
+        <h3>{songs.release_date}</h3>
+        <h3>{songs.genre}</h3>
+      </div>)}
+    </div>  
   );
 }
 
